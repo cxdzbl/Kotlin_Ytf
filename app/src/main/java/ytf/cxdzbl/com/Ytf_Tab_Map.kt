ce46e09e671b038786e79e3dd7fafd3787786bff
cxdzbl <848563110@qq.com>
2018-08-03 08:41:18 +0800

    更新代码
    Signed-off-by: cxdzbl <848563110@qq.com>

--- app/src/main/java/ytf/cxdzbl/com/Ytf_Tab_Map.kt
+++ app/src/main/java/ytf/cxdzbl/com/Ytf_Tab_Map.kt
@@ -80,7 +80,7 @@ var andm_y:Float? =null
 class Ytf_Tab_Map : Fragment(){
     var myListener =MyLocationListenner()
     var cxd=5
-    val cxdddddd:String="陈向东"
+
 //住码云上更新代码测试
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
